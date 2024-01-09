d521d882a315b0b905a42fef0fc5f1ada9be6334
HsiaoHungWang <wang@ispan.com.tw>
2024-01-09 15:18:23 +0800

    透過 axios 串接 api

--- src/views/Spots.vue
+++ src/views/Spots.vue
@@ -3,7 +3,15 @@
 </template>
     
 <script setup>
-    
+    import axios from 'axios'
+import { onMounted } from 'vue';
+
+    onMounted(async ()=>{
+     // const response = await axios.get('http://localhost:8080/api/spot/categories')
+     const response = await axios.get(`${import.meta.env.VITE_API_SPOTURL}/spot/categories`)
+      console.log(response.data)
+      console.log(import.meta.env.VITE_API_SPOTURL)
+    })
 </script>
     
 <style scoped>
