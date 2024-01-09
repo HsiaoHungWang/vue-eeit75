ad90fceb6ac585505ec4d49f35fa90e3976a24ae
HsiaoHungWang <wang@ispan.com.tw>
2024-01-09 12:03:03 +0800

    建立三個 Vuew Component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup>
    import First from './components/First.vue'
+   import Home from './views/Home.vue'
+   import About from './views/About.vue'
+   import Contact from './views/Contact.vue'
 </script>
 
 <template>
   <h2>Hello Vue!!</h2>
   <First></First>
+  <Home></Home>
+  <About></About>
+  <Contact></Contact>
+  
 </template>
 
 <style scoped>
